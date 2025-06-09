0222e6f2693c318bc950ac9c6d9d8f18c8bdacfc
Vinícius Guimarães <vinigleandro@gmail.com>
2025-06-09 19:40:51 -0300

    Added dashboardGrid

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,6 +5,7 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialsAlert: "[role='alert']"
   }
 
@@ -14,7 +15,7 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.passwordField).type("admin123")
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
 
   it('Login - fail', () => {
