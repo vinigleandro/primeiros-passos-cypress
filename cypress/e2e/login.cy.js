669cc2c66f091e9c5b8c98739e24781f2cc7986e
Vinícius Guimarães <vinigleandro@gmail.com>
2025-06-26 11:26:35 -0300

    Renaming folder

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,8 +13,8 @@ describe('Orange HRM Tests', () => {
 
   it('Login - success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
